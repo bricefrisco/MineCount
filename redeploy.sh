6c0cc47fc706faefa9605faf02386ae1fc136a22
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2022-11-22 21:19:50 -0600

    Added auto restart to redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -3,8 +3,8 @@ docker pull bricefrisco/minecount-be:latest
 docker pull bricefrisco/minecount-fe:latest
 docker stop minecount-be
 docker rm minecount-be
-docker run -d -p 8080:8080 --memory=512m --cpus=0.5 --name=minecount-be bricefrisco/minecount-be:latest --env-file /etc/minecount/minecount-be.env
+docker run -d --restart=unless-stopped -p 8080:8080 --memory=512m --cpus=0.5 --name=minecount-be bricefrisco/minecount-be:latest --env-file /etc/minecount/minecount-be.env
 docker stop minecount-fe
 docker rm minecount-fe
-docker run -d -p 3000:3000 --memory=512m --cpus=0.5 --name=minecount-fe bricefrisco/minecount-fe:latest --env-file /etc/minecount/minecount-fe.env
+docker run -d --restart=unless-stopped -p 3000:3000 --memory=512m --cpus=0.5 --name=minecount-fe bricefrisco/minecount-fe:latest --env-file /etc/minecount/minecount-fe.env
 docker system prune -f
