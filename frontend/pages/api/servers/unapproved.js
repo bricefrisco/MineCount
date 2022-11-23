808d88afdbd49aeb0fd538116c9ea4fc325040be
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2022-11-22 21:13:10 -0600

    Fixed url paths

--- frontend/pages/api/servers/unapproved.js
+++ frontend/pages/api/servers/unapproved.js
@@ -3,12 +3,15 @@ import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
 export default withApiAuthRequired(async (req, res) => {
   const { accessToken } = await getAccessToken(req, res);
 
-  const response = await fetch(`${process.env.BACKEND_URI}/unapproved`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+  const response = await fetch(
+    `${process.env.BACKEND_URI}/servers/unapproved`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
 
   const data = await response.json();
 
