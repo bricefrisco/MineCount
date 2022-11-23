e61f6faf93d960ae5968bc49c5e1a5c496fdbab7
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2022-11-22 21:13:01 -0600

    Update redeployment script

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 docker pull bricefrisco/minecount-be:latest
 docker pull bricefrisco/minecount-fe:latest
-docker system prune -f
+docker stop minecount-be
+docker rm minecount-be
 docker run -d -p 8080:8080 --memory=512m --cpus=0.5 --name=minecount-be bricefrisco/minecount-be:latest --env-file /etc/minecount/minecount-be.env
+docker stop minecount-fe
+docker rm minecount-fe
 docker run -d -p 3000:3000 --memory=512m --cpus=0.5 --name=minecount-fe bricefrisco/minecount-fe:latest --env-file /etc/minecount/minecount-fe.env
+docker system prune -f
